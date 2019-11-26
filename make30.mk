362cc27069c3d623e9ecee72af0028ce081666ec
vblandon10 <42391104+vblandon10@users.noreply.github.com>
2019-11-26 13:48:47 -0500

    Delete make30.mk

--- make30.mk
+++
@@ -1,8 +0,0 @@
-grafica.png : grafica.py dat30.dat
-	python grafica.py 
-dat30.dat : a.out
-	./a.out > dat30.dat
-a.out : BlandonValentina_Ejercicio30.cpp
-	c++ BlandonValentina_Ejercicio30.cpp
-
-    
